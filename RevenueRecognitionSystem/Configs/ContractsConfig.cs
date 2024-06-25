114eb0a0d9eb8733a70975fc90cdfbe60a714d94
bredecorne <kowalewskijakub@icloud.com>
2024-06-25 12:27:14 +0200

    modify on delete action

--- RevenueRecognitionSystem/Configs/ContractsConfig.cs
+++ RevenueRecognitionSystem/Configs/ContractsConfig.cs
@@ -8,6 +8,24 @@ public class ContractsConfig : IEntityTypeConfiguration<Contract>
 {
     public void Configure(EntityTypeBuilder<Contract> builder)
     {
+        builder
+            .HasOne(c => c.Client)
+            .WithMany(c => c.Contracts)
+            .HasForeignKey(c => c.ClientId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(c => c.Software)
+            .WithMany()
+            .HasForeignKey(c => c.SoftwareId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .HasOne(c => c.SoftwareVersion)
+            .WithMany()
+            .HasForeignKey(c => c.SoftwareVersionId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
         var contracts = new[]
         {
             new Contract(1, DateTime.Parse("2024-01-01"), DateTime.Parse("2024-03-30"),
